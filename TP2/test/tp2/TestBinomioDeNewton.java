1b06b845865f217241463f8aabe3b6a4574e2be3
Joaquin Oria <joria@smata.com.ar>
2017-09-19 11:22:54 -0300

    Se agregaron mas casos de prueba en el test de BinomioDeNewton.

--- TP2/test/tp2/TestBinomioDeNewton.java
+++ TP2/test/tp2/TestBinomioDeNewton.java
@@ -11,6 +11,9 @@ public class TestBinomioDeNewton {
 	BinomioDeNewton b4;
 	BinomioDeNewton b5;
 	BinomioDeNewton b6;
+	BinomioDeNewton b7;
+	BinomioDeNewton b8;
+	BinomioDeNewton b9;
 	
 	@Before
 	public void setUp() {
@@ -20,6 +23,9 @@ public class TestBinomioDeNewton {
 		b4 = new BinomioDeNewton(2,-3,7);  // 128 x^7 - 1344 x^6 + 6048 x^5 - 15120 x^4 + 22680 x^3 - 20412 x^2 + 10206 x - 2187
 		b5 = new BinomioDeNewton(-1,-4,9); // -x^9 - 36 x^8 - 576 x^7 - 5376 x^6 - 32256 x^5 - 129024 x^4 - 344064 x^3 - 589824 x^2 - 589824 x - 262144
 		b6 = new BinomioDeNewton(-3,2,15); //-14348907 x^15 + 143489070 x^14 - 669615660 x^13 + 1934445240 x^12 - 3868890480 x^11 + 5674372704 x^10 - 6304858560 x^9 + 5404164480 x^8 - 3602776320 x^7 + 1868106240 x^6 - 747242496 x^5 + 226437120 x^4 - 50319360 x^3 + 7741440 x^2 - 737280 x + 32768
+		b7 = new BinomioDeNewton(-4,3,17);
+		b8 = new BinomioDeNewton(-4,3,19);
+		b9 = new BinomioDeNewton(-4,3,25);
 	}
 	
 	
@@ -31,5 +37,8 @@ public class TestBinomioDeNewton {
 		Assert.assertEquals(6048, b4.obtenerCoeficiente(5),0.001);
 		Assert.assertEquals(-36, b5.obtenerCoeficiente(8),0.001);
 		Assert.assertEquals(-14348907, b6.obtenerCoeficiente(15),0.001);
+		Assert.assertEquals(Double.parseDouble("-17179869184"), b7.obtenerCoeficiente(17),0.001);
+		Assert.assertEquals(Double.parseDouble("-274877906944"), b8.obtenerCoeficiente(19),0.001);
+		Assert.assertEquals(Double.parseDouble("-1125899906842624"), b9.obtenerCoeficiente(25),0.001);
 	}
 }
